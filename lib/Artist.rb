6bde0aa87ec8e1cb83ed91d4da17ea1acfb94f0c
ronald vilorio <soccer23415@gmail.com>
2017-12-07 20:43:46 +0000

    initialize,attr accessor name and @@all array for Genre,fi

--- lib/Artist.rb
+++ lib/Artist.rb
@@ -15,4 +15,13 @@ class Artist
   def self.destroy_all
     @@all.clear
   end
+
+  def save
+    @@all << self
+  end
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
 end
