d0789a92b808e6b6fa0d3c557ed5936da428ee38
ronald vilorio <soccer23415@gmail.com>
2017-12-08 19:52:03 +0000

    added initialize,path and files methods on MusicImporter class

---
+++ lib/musicImporter.rb
@@ -0,0 +1,16 @@
+require 'pry'
+class MusicImporter
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    Dir.entries(@path).select{|file|
+      file.include?('.mp3')
+    }
+    
+
+  end
+end
