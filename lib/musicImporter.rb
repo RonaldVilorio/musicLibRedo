8722c815df3b4c647a936cb2ce8d25935237f191
ronald vilorio <soccer23415@gmail.com>
2017-12-09 17:44:03 +0000

    added import method to musicImporter class

--- lib/musicImporter.rb
+++ lib/musicImporter.rb
@@ -10,7 +10,9 @@ class MusicImporter
     Dir.entries(@path).select{|file|
       file.include?('.mp3')
     }
-    
 
+  end
+  def import
+    files.each{|file| Song.create_from_filename(file)}
   end
 end
